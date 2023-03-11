9661228e944409af16523ef4ceab9e03a09ec306
Ananta Moharana <anantamoharana@macbook-pro.myfiosgateway.com>
2023-03-11 18:40:36 -0500

    Successfully able to get the songs from the desired playlist

--- dags/spotifydag.py
+++ dags/spotifydag.py
@@ -4,7 +4,7 @@ import pandas as pd
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from src.extract import authenitcate_api
+from src.extract import authenitcate_api, get_songs_from_playlist
 
 
 #Create default argument for dag
@@ -14,7 +14,7 @@ default_args = {
     'depends_on_past': False,
     'catchup': False,
     'retries': 2,
-    'retry_delay': timedelta(minutes = 2)
+    'retry_delay': timedelta(minutes= .5)
 }
 
 #Create dag instance
@@ -22,16 +22,16 @@ dag = DAG(
     'Spotify_Data_ETL',
     default_args = default_args,
     description = 'Gets data regarding the Top 50 Songs in the World and their artists',
-    schedule_interval = '@monthly'
+    schedule = '@monthly'
     )
 
 #Create start etl task
 start_etl = DummyOperator(
-    task_id = 'StartSETL',  
+    task_id = 'StartETL',  
     dag = dag
 )
 
-#Create the get songs from a playlist tag
+#Create the authentication operator
 authenitcate=PythonOperator(
     task_id='AuthenticateAPI',
     python_callable=authenitcate_api,
@@ -39,5 +39,15 @@ authenitcate=PythonOperator(
 
 )
 
+#Create the operataro to get the songs from the playlist
+get_songs=PythonOperator(
+    task_id='GetSongsFromPlaylist',
+    python_callable=get_songs_from_playlist,
+    dag=dag
 
-start_etl >> authenitcate
+)
+
+
+
+
+start_etl >> authenitcate >> [get_songs]
--- dags/src/extract.py
+++ dags/src/extract.py
@@ -24,6 +24,58 @@ def authenitcate_api(ti):
     # save the access token
     access_token = auth_response_data['access_token']
 
+    print(access_token)
+
     ti.xcom_push(key='access_token', value=access_token)
 
 
+#Get the songs present in the Top 50 Global Playlist https://open.spotify.com/playlist/37i9dQZEVXbMDoHDwVN2tF
+def get_songs_from_playlist(ti):
+
+    #Get the access token fron xcom
+    access_token=ti.xcom_pull(key='access_token')
+
+    #Headers for making the API call
+    headers = {'Authorization': 'Bearer {token}'.format(token=access_token)}
+
+    #Base URL of all Spotify API endpoints
+    BASE_URL = 'https://api.spotify.com/v1/'
+
+    #Playlist ID from the URI
+    playlist_id = '37i9dQZEVXbMDoHDwVN2tF'
+
+    #print("This part takes too long")
+    #GET request to get the playlist items from the endpoint
+    r = requests.get(BASE_URL + 'playlists/' + playlist_id + '/tracks', headers=headers)
+
+    #Convert the response to a json
+    r = r.json()
+
+    r
+
+    #Create a dataframe to hold all the information
+    songs_df = pd.DataFrame(columns=['song_name', 'track_id', 'is_explicit', 'popularity', 'artist_name','artist_id'])
+
+    #Loop through the song items in the json
+    for item in r['items']:
+        #Get the song name
+        song_name=item['track']['name']
+        #Get the song id
+        track_id=item['track']['id']
+        #Get whether or not the song is explicit
+        is_explicit=item['track']['explicit']
+        #Get the songs popularity
+        poplarity=item['track']['popularity']
+
+        for artist in item['track']['artists']:
+            #Get the artist name
+            artist_name=artist['name']
+            #Get the artist id
+            artist_id=artist['id']
+            #Create a new dataframe row and then append it
+            new_row = pd.DataFrame({'song_name':song_name,'track_id':track_id,'is_explicit':is_explicit, 'popularity':poplarity, 'artist_name':artist_name,"artist_id":artist_id}, index=[0])
+            songs_df = pd.concat([new_row,songs_df.loc[:]]).reset_index(drop=True)
+
+    ti.xcom_push(key='songs_df', value=songs_df.to_json())
+
+#Get the audio attributes of the songs
